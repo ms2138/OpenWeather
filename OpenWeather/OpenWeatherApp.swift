de8421a631721f35d83d3fac03bf7e36aa0f47ca
ms2138 <m38371@gmail.com>
2021-06-06 21:44:43 -0700

    Initial commit

---
+++ OpenWeather/OpenWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherApp.swift
+//  OpenWeather
+//
+//  Created by mani on 2021-06-06.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
