5f8784e48a381f8ca0421dd7f737f6881674a085
ms2138 <m38371@gmail.com>
2021-06-16 19:58:06 -0700

    Add getFiveDayWeatherForecast, getSevenDayWeatherForecast, getCurrentWeatherForecast, and getLocations methods to WeatherFetchable protocol

--- OpenWeather/WeatherAPI/WeatherFetchable.swift
+++ OpenWeather/WeatherAPI/WeatherFetchable.swift
@@ -6,7 +6,25 @@
 //
 
 import Foundation
+import Combine
 
 protocol WeatherFetchable {
+    func getFiveDayWeatherForecast(
+        forCoordinates coord: Coordinates,
+        unit: TemperatureUnit
+    ) -> AnyPublisher<FiveDayWeatherForecast, APIError>
 
+    func getSevenDayWeatherForecast(
+        forCoordinates coord: Coordinates,
+        unit: TemperatureUnit
+    ) -> AnyPublisher<SevenDayWeatherForecast, APIError>
+
+    func getCurrentWeatherForecast(
+        forCoordinates coord: Coordinates,
+        unit: TemperatureUnit
+    ) -> AnyPublisher<CurrentWeatherForecast, APIError>
+
+    func getLocations (
+        forCity city: String
+    ) -> AnyPublisher<Location, APIError>
 }
