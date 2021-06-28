9c7078dfa40672f43097acd2504c366b4e058932
ms2138 <m38371@gmail.com>
2021-06-27 19:44:28 -0700

    Add View Models group.  Add LocationViewModel class to View Models group

---
+++ OpenWeather/View Models/LocationViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LocationViewModel.swift
+//  OpenWeather
+//
+//  Created by mani on 2021-06-27.
+//
+
+import Foundation
+import Combine
+
+class LocationViewModel: ObservableObject {
+
+}
