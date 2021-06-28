d8484fd63e082ef0004b08a97e77e319bcf22da1
ms2138 <m38371@gmail.com>
2021-06-27 19:40:57 -0700

    Rename SearchBarView to AddCityView

--- OpenWeather/AddCityView.swift
+++ OpenWeather/AddCityView.swift
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct SearchBarView: View {
+struct AddCityView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct SearchBarView_Previews: PreviewProvider {
+struct AddCityView_Previews: PreviewProvider {
     static var previews: some View {
-        SearchBarView()
+        AddCityView()
     }
 }
