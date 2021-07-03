bd60dad3f5d057226466184f62e2ccc86a5af2ae
ms2138 <m38371@gmail.com>
2021-07-02 21:29:37 -0700

    Add WeeklyWeatherViewModel class

---
+++ OpenWeather/View Models/WeeklyWeatherViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  WeeklyWeatherViewModel.swift
+//  OpenWeather
+//
+//  Created by mani on 2021-07-02.
+//
+
+import Foundation
+import Combine
+
+class WeeklyWeatherViewModel: ObservableObject {
+    
+}
