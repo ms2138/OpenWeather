5edbaa2e22e937e813f77e4fee47af2a3a137a25
ms2138 <m38371@gmail.com>
2021-07-02 20:43:15 -0700

    Add WeatherDetailView

---
+++ OpenWeather/Views/WeatherDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherDetailView.swift
+//  OpenWeather
+//
+//  Created by mani on 2021-07-02.
+//
+
+import SwiftUI
+
+struct WeatherDetailView: View {
+    var body: some View {
+        Text("Weather")
+    }
+}
+
+struct WeatherDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherDetailView()
+    }
+}
