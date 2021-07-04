9b60334ee8af446660410b39faa9817e8f215bf4
ms2138 <m38371@gmail.com>
2021-07-03 23:46:39 -0700

    Add disposables property and currentWeather method to CurrentWeatherViewModel

--- OpenWeather/View Models/CurrentWeatherViewModel.swift
+++ OpenWeather/View Models/CurrentWeatherViewModel.swift
@@ -11,4 +11,21 @@ import Combine
 class CurrentWeatherViewModel: ObservableObject {
     var weather = WeatherAPI()
     @Published var dataSource = [CurrentWeatherForecast]()
+    private var disposables = Set<AnyCancellable>()
+
+    func currentWeather(forCoordinates coord: Coordinates) {
+        weather.getCurrentWeatherForecast(forCoordinates: coord)
+            .receive(on: DispatchQueue.main)
+            .sink { (value) in
+                switch value {
+                    case .failure(_):
+                        print("Request failed: \(value)")
+                    case .finished:
+                        break
+                }
+            } receiveValue: { (forecast) in
+                self.dataSource.append(forecast)
+            }
+            .store(in: &disposables)
+    }
 }
