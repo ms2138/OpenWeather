7d37563f7703099f25a713dfe9c45cb36e3910cf
ms2138 <m38371@gmail.com>
2021-07-03 20:11:21 -0700

    Add icons, maxTemperature and minTemperature properties

--- OpenWeather/View Models/WeatherRowViewModel.swift
+++ OpenWeather/View Models/WeatherRowViewModel.swift
@@ -10,4 +10,29 @@ import Foundation
 struct WeatherRowViewModel: Identifiable {
     private let forecast: Forecast
     var id: UUID = UUID()
+
+    var icons: String {
+        switch forecast.weather[0].main {
+            case .snow:
+                return "🌨"
+            case .clear:
+                return "☀️"
+            case .clouds:
+                return "🌥"
+            case .thunderstorm:
+                return "⛈"
+            case .drizzle:
+                return "🌦"
+            case .rain:
+                return "🌧"
+        }
+    }
+
+    var maxTemperature: String {
+        return String(format: "%.1f", forecast.temp.max)
+    }
+
+    var minTemperature: String {
+        return String(format: "%.1f", forecast.temp.min)
+    }
 }
