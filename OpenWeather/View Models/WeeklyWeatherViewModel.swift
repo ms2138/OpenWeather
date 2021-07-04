b1415b72e75d664510340b7f3fd80ac4310f1bdf
ms2138 <m38371@gmail.com>
2021-07-03 21:31:34 -0700

    Add weather, datasource, and disposables properties.  Add weaklyWeather method to WeeklyWeatherViewModel

--- OpenWeather/View Models/WeeklyWeatherViewModel.swift
+++ OpenWeather/View Models/WeeklyWeatherViewModel.swift
@@ -9,5 +9,26 @@ import Foundation
 import Combine
 
 class WeeklyWeatherViewModel: ObservableObject {
-    
+    var weather = WeatherAPI()
+    @Published var dataSource: [WeatherRowViewModel] = []
+    private var disposables = Set<AnyCancellable>()
+
+    func weeklyWeather(forCoordinates coord: Coordinates) {
+        weather.getSevenDayWeatherForecast(forCoordinates: coord)
+            .map {
+                $0.forecast.map(WeatherRowViewModel.init)
+            }
+            .receive(on: DispatchQueue.main)
+            .sink { (value) in
+                switch value {
+                    case .finished:
+                        break
+                    case .failure(_):
+                        print("Request failed: \(value)")
+                }
+            } receiveValue: { (forecast) in
+                self.dataSource = forecast
+            }
+            .store(in: &disposables)
+    }
 }
