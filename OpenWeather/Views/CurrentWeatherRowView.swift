63e5c19ed2618f4f21fb10254c60e53ab5740b68
ms2138 <m38371@gmail.com>
2021-07-04 19:57:03 -0700

    Add weather properties to CurrentWeatherRowView and CurrentWeatherRowView_Previews.  Add text view components to display name, description and temperature in CurrentWeatherRowView

--- OpenWeather/Views/CurrentWeatherRowView.swift
+++ OpenWeather/Views/CurrentWeatherRowView.swift
@@ -8,13 +8,31 @@
 import SwiftUI
 
 struct CurrentWeatherRowView: View {
+    let weather: CurrentWeatherForecast
+
     var body: some View {
-        Text("Row")
+        HStack {
+            VStack {
+                Text("\(weather.name)")
+                Text("\(weather.weather.first!.weatherDescription)")
+                    .autocapitalization(.words)
+                    .font(.caption2)
+            }
+            Spacer()
+            Text("\(String(format: "%.1f", weather.main.temperature))")
+                .font(.title)
+        }
     }
 }
 
 struct CurrentWeatherRowView_Previews: PreviewProvider {
+    @State static var weather = CurrentWeatherForecast(id: 19282, name: "Madison", coord: Coord(lat: 12.3, lon: 12.3),
+                                                       weather: [CurrentWeatherForecast.Weather(id: 3838, main: "Clear",
+                                                                         weatherDescription: "clear sky", icon: "01d")],
+                                                       main: .init(temperature: 23.4, humidity: 3,
+                                                                   maxTemperature: 25.0, minTemperature: 18.0))
+
     static var previews: some View {
-        CurrentWeatherRowView()
+        CurrentWeatherRowView(weather: weather)
     }
 }
