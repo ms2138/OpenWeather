0ac2d7b5aec8e144403a25b55e3996abe64620be
ms2138 <m38371@gmail.com>
2021-07-04 19:17:39 -0700

    Add CurrentWeatherRowView

---
+++ OpenWeather/Views/CurrentWeatherRowView.swift
@@ -0,0 +1,20 @@
+//
+//  CurrentWeatherRowView.swift
+//  OpenWeather
+//
+//  Created by mani on 2021-07-04.
+//
+
+import SwiftUI
+
+struct CurrentWeatherRowView: View {
+    var body: some View {
+        Text("Row")
+    }
+}
+
+struct CurrentWeatherRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CurrentWeatherRowView()
+    }
+}
