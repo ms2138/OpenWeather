88b5ddabdc8099696fb30d1ed875fa2bc10534e9
ms2138 <m38371@gmail.com>
2021-07-05 21:47:23 -0700

    Add viewModel property and init method to WeeklyWeatherRowView.  Add viewModel static property to WeeklyWeatherRowView_Previews

--- OpenWeather/Views/WeeklyWeatherRowView.swift
+++ OpenWeather/Views/WeeklyWeatherRowView.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct WeeklyWeatherRowView: View {
+    private let viewModel: WeeklyWeatherViewModel
+
+    init(viewModel: WeeklyWeatherViewModel) {
+        self.viewModel = viewModel
+    }
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 struct WeeklyWeatherRowView_Previews: PreviewProvider {
+    @State static var viewModel = WeeklyWeatherViewModel()
+
     static var previews: some View {
-        WeeklyWeatherRowView()
+        WeeklyWeatherRowView(viewModel: viewModel)
     }
 }
