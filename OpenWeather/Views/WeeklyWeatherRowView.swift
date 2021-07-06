f79993194588a34d9439a71b47240c4904d5c55f
ms2138 <m38371@gmail.com>
2021-07-05 21:43:56 -0700

    Add WeeklyWeatherRowView

---
+++ OpenWeather/Views/WeeklyWeatherRowView.swift
@@ -0,0 +1,20 @@
+//
+//  WeeklyWeatherRowView.swift
+//  OpenWeather
+//
+//  Created by mani on 2021-07-05.
+//
+
+import SwiftUI
+
+struct WeeklyWeatherRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeeklyWeatherRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeeklyWeatherRowView()
+    }
+}
