0aeba1bbab475d5ce8b19bf4d3ff88790cf78dfe
ms2138 <m38371@gmail.com>
2021-07-06 22:26:23 -0700

    Add viewModel property to WeeklyWeatherRowView.  Add Text views to display day, icon, description, maxTemperature and minTemperature.  Add static property to WeeklyWeatherRowView_Previews

--- OpenWeather/Views/WeeklyWeatherRowView.swift
+++ OpenWeather/Views/WeeklyWeatherRowView.swift
@@ -8,19 +8,53 @@
 import SwiftUI
 
 struct WeeklyWeatherRowView: View {
-    private let viewModel: WeeklyWeatherViewModel
+    private let viewModel: WeatherRowViewModel
 
-    init(viewModel: WeeklyWeatherViewModel) {
+    init(viewModel: WeatherRowViewModel) {
         self.viewModel = viewModel
     }
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack(alignment: .leading) {
+                Text("\(viewModel.day)")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+
+            VStack(alignment: .leading) {
+                Text("\(viewModel.icon)")
+                    .font(.body)
+                    .frame(maxWidth: .infinity)
+
+                Text("\(viewModel.description)")
+                    .font(.footnote)
+                    .frame(maxWidth: .infinity)
+
+            }
+
+            HStack(alignment: .center, spacing: 10.0) {
+                Text("\(viewModel.maxTemperature)°")
+                    .font(.caption)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                Text("\(viewModel.minTemperature)°")
+                    .font(.caption)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+            }
+        }
     }
 }
 
 struct WeeklyWeatherRowView_Previews: PreviewProvider {
-    @State static var viewModel = WeeklyWeatherViewModel()
+    @State static var viewModel = WeatherRowViewModel(forecast: Forecast(date: Date(), sunrise: Date(),
+                                                                         sunset: Date(), moonrise: Date(), moonset: Date(),
+                                                                         moonPhase: 23.0, temp: Temp(day: 23.0, min: 23.0, max: 23.0,
+                                                                                                     night: 23.0, eve: 23.0, morn: 23.0),
+                                                                         feelsLike: FeelsLike(day: 12.0, night: 12.0, eve: 23.0, morn: 23.0),
+                                                                         pressure: 12, humidity: 12, dewPoint: 21.2, windSpeed: 23.3,
+                                                                         windDeg: 32, windGust: 34.3, weather:
+                                                                            [Weather(id: 393, main: .clear,
+                                                                                     weatherDescription: "clear sky", icon: "39d")],
+                                                                         clouds: 12, pop: 293.9, rain: nil, uvi: 13))
 
     static var previews: some View {
         WeeklyWeatherRowView(viewModel: viewModel)
