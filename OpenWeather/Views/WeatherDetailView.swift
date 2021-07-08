9e1aae6d8dfa4378ca5b75dcd176802d7e540fff
ms2138 <m38371@gmail.com>
2021-07-07 22:19:13 -0700

    Add sunInfoSection, rainInfoSection, and additionalInfoSection properties to WeatherDetailView.  Add Section that displays sunInfo, rainInfo, and additionalInfo

--- OpenWeather/Views/WeatherDetailView.swift
+++ OpenWeather/Views/WeatherDetailView.swift
@@ -24,6 +24,11 @@ struct WeatherDetailView: View {
             case .loaded:
                 List {
                     forecastSection
+                    Section {
+                        sunInfoSection
+                        rainInfoSection
+                        additionalInfoSection
+                    }
                 }
         }
     }
@@ -35,7 +40,56 @@ private extension WeatherDetailView {
             ForEach(viewModel.dataSource, content: WeeklyWeatherRowView.init(viewModel:))
         }
     }
+
+    var sunInfoSection: some View {
+        VStack {
+            if let firstForecast = viewModel.dataSource.first {
+                HStack {
+                    WeatherInfoView(title: "SUNRISE", information: "\(firstForecast.sunrise)")
+                    WeatherInfoView(title: "SUNSET", information: "\(firstForecast.sunset)")
+                }
+            }
+        }
+    }
+
+    var rainInfoSection: some View {
+        VStack {
+            if let firstForecast = viewModel.dataSource.first {
+                HStack {
+                    WeatherInfoView(title: "CHANCE OF RAIN", information: "\(firstForecast.precipitation)")
+                    WeatherInfoView(title: "HUMIDITY", information: "\(firstForecast.humidity)")
+                }
+            }
+        }
+    }
+
+    var additionalInfoSection: some View {
+        VStack {
+            if let firstForecast = viewModel.dataSource.first {
+                HStack {
+                    WeatherInfoView(title: "UV", information: "\(firstForecast.uvIndex)")
+                    WeatherInfoView(title: "FEELS LIKE", information: "\(firstForecast.feelsLike)")
+                }
+            }
+        }
+    }
 }
+
+struct WeatherInfoView: View {
+    var title: String
+    var information: String
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.caption2)
+            Text(information)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
+
+
 
 struct WeatherDetailView_Previews: PreviewProvider {
     @State static var coord = Coordinates(latitude: 23.0, longitude: 45.0)
