6bccdba19bafdd20180609de0ca375a0f0681f71
ms2138 <m38371@gmail.com>
2021-07-13 21:11:17 -0700

    Add convert temperature method to TemperatureConversion

--- OpenWeather/Helpers/TemperatureConversion.swift
+++ OpenWeather/Helpers/TemperatureConversion.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+func convert(temperature temp: Double, to unit: TemperatureUnit) -> Double {
+    switch unit {
+        case .celsius:
+            return temp - 273.15
+        case .fahrenheit:
+            return (temp * 1.8) - 459.67
+        default:
+            return 0.0
+    }
+}
