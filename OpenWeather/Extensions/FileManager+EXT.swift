dbd7f72956404299509179c3eb7dc333f6a8392e
ms2138 <m38371@gmail.com>
2021-07-14 21:53:00 -0700

    Add documentsDirectory property to FileManager extension

--- OpenWeather/Extensions/FileManager+EXT.swift
+++ OpenWeather/Extensions/FileManager+EXT.swift
@@ -8,5 +8,14 @@
 import Foundation
 
 extension FileManager {
-
+    var documentsDirectory: URL {
+        do {
+            return try FileManager.default.url(for: .documentDirectory,
+                                               in: .userDomainMask,
+                                               appropriateFor: nil,
+                                               create: false)
+        } catch {
+            fatalError("Failed to find documents directory.")
+        }
+    }
 }
