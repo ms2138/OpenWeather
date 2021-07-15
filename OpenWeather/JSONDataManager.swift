e1110b5407b7479aaa0e2721e6e197c7f705a3c4
ms2138 <m38371@gmail.com>
2021-07-14 21:16:48 -0700

    Add write and read method to JSONDataManager.  Add generic that conforms to Codable

--- OpenWeather/JSONDataManager.swift
+++ OpenWeather/JSONDataManager.swift
@@ -7,5 +7,15 @@
 
 import Foundation
 
-class JSONDataManager {
+class JSONDataManager<T: Codable> {
+    func write(data: T, to url: URL) throws {
+        let encodedFeeds = try JSONEncoder().encode(data)
+        try encodedFeeds.write(to: url)
+    }
+
+    func read(from url: URL) throws -> [T] {
+        let savedData = try Data(contentsOf: url)
+        let decodedData = try JSONDecoder().decode([T].self, from: savedData)
+        return decodedData
+    }
 }
