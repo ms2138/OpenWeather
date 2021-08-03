2f906142fd0cd1a02fd7e6291c7024108b87712d
ms2138 <m38371@gmail.com>
2021-08-02 20:19:55 -0700

    Add a check when a new city is entered to make sure it isn't an empty string or only spaces.  Add state property to track changes when making a network request in LocationViewModel

--- OpenWeather/View Models/LocationViewModel.swift
+++ OpenWeather/View Models/LocationViewModel.swift
@@ -12,28 +12,39 @@ class LocationViewModel: ObservableObject {
     var weather = WeatherAPI()
     @Published var city: String = ""
     @Published var dataSource: [LocationElement] = []
+    @Published private(set) var state = LoadingState.idle
     private var disposables = Set<AnyCancellable>()
 
     init() {
         $city
             .dropFirst(1) // Drop the first value because it will perform a search for an empty city
-            .debounce(for: .seconds(1.0), scheduler: DispatchQueue(label: "LocationViewModel"))
-            .sink(receiveValue: locations(for:))
+            .debounce(for: .seconds(0.8), scheduler: DispatchQueue(label: "LocationViewModel"))
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: { [weak self] city in
+                if !city.isEmpty && city.trimmingCharacters(in: .whitespaces) != "" {
+                    self?.locations(for: city)
+                } else {
+                    self?.state = .idle
+                }
+            })
             .store(in: &disposables)
     }
 
     func locations(for string: String) {
+        self.state = .loading
+        
         weather.getLocations(forCity: city)
             .receive(on: DispatchQueue.main)
-            .sink { (value) in
+            .sink { [weak self] value in
                 switch value {
                     case .finished:
                         break
-                    case .failure(_):
-                        print("Request failed: \(value)")
+                    case .failure(let error):
+                        self?.state = .failed(error)
                 }
-            } receiveValue: { (location) in
-                self.dataSource = location
+            } receiveValue: { [weak self] location in
+                self?.state = .loaded
+                self?.dataSource = location
             }
             .store(in: &disposables)
     }
